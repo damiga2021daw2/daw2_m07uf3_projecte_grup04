1ea51b776d7b66708b2f2f758dd9133155d07e15
damiga2021daw2 <damiga2021daw2@gmail.com>
2021-04-15 12:24:28 +0200

    Scripts arreglats

--- script1.sql
+++ script1.sql
@@ -1,3 +1,6 @@
+use mysql;
+create user 'admin'@'localhost' identified by "Fjeclot21@";
 drop database if exists gong;
 create database gong;
 use gong;
+grant all privileges on gong.* to 'admin'@'localhost';
--- script2.sql
+++ script2.sql
@@ -1,4 +1,6 @@
-INSERT INTO usuari VALUES ('SysAdmin',MD5('Qwerty1'),'System','Administrator','GONGsysAdmin@gmail.com','636589632',TRUE);
+use gong;
+INSERT INTO usuaris VALUES ('SysAdmin',MD5('Qwerty1'),'System','Administrator',
+'GONGsysAdmin@gmail.com','636589632',TRUE);
 
 CREATE USER IF NOT EXISTS 'SysAdmin'@'localhost' IDENTIFIED BY 'Qwerty1';
 GRANT ALL ON gong.* TO 'SysAdmin'@'localhost';
